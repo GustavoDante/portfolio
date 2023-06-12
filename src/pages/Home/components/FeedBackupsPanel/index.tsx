af21c9e250b6799fc00fc28fcdddf2baa2db1889
GustavoDante <gustavodantespfc@gmail.com>
2023-06-12 09:33:06 -0300

    chore: text of feddback changed

--- src/pages/Home/components/FeedBackupsPanel/index.tsx
+++ src/pages/Home/components/FeedBackupsPanel/index.tsx
@@ -12,11 +12,15 @@ export function FeedBackupsPanel() {
         </p>
       </div>
       <div>
-        <h3>Arthur Brito (UX/UI Designer | Product Designer)</h3>
+        <h3>Luis Manoel (Desenvolvedor Web FullStack)</h3>
         <p>
-          Um profissional muito habilidoso, que sabe trabalhar colaborativamente
-          com o time e que sempre está buscando aprender e evoluir os seus
-          conhecimentos na área do desenvolvimento.
+          Excelente profissional e colega de trabalho, sempre disposto a ajudar
+          e esclarecer dúvidas da equipe. Um desenvolvedor dedicado, que busca
+          constantemente aprimorar seus conhecimentos, aplicando-os de forma
+          eficaz nos projetos e compartilhando-os com os demais. Além disso,
+          possui um ótimo conhecimento técnico e uma ótima habilidade em
+          aprender novas tecnologias de forma eficiente, o que o capacita a
+          enfrentar com sucesso novos desafios.
         </p>
       </div>
       {/* <div>
